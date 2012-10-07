89cb7cc118fbce7b09ea73ff9c807bf2ac76196e
Thomas Schuetz <ts@protos.de>
2012-10-07 21:02:18 +0200

    [tutorials.simulators.trafficlight] added EPL header

--- examples/org.eclipse.etrice.tutorials.simulators.trafficlight/src/org/eclipse/etrice/tutorials/simulators/trafficlight/ButtonActionListener.java
+++ examples/org.eclipse.etrice.tutorials.simulators.trafficlight/src/org/eclipse/etrice/tutorials/simulators/trafficlight/ButtonActionListener.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2012 tieto deutschland gmbh (http://www.tieto.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thomas Jung
+ *******************************************************************************/ 
+
+
 package org.eclipse.etrice.tutorials.simulators.trafficlight;
 
 
