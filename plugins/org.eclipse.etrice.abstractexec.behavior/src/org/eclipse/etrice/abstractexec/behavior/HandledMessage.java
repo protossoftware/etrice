c1ca50b9bc9aa757ae4afa1110fdd02f77c699b1
Rohit Agrawal <rohi0012@e.ntu.edu.sg>
2012-12-18 12:50:05 +0530

    [CQ6589] :  Added copyright notes. 1.	I authored 100% of the content I contributed 2.	I have the rights to donate the content to Eclipse 3.	I contribute the content under the EPL
    Change-Id: I7ec5fb774bdb5062b06c9ecc7fb293f12e5b871a

--- plugins/org.eclipse.etrice.abstractexec.behavior/src/org/eclipse/etrice/abstractexec/behavior/HandledMessage.java
+++ plugins/org.eclipse.etrice.abstractexec.behavior/src/org/eclipse/etrice/abstractexec/behavior/HandledMessage.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 protos software gmbh (http://www.protos.de).
- * All rights reserved. This program and the accompanying materials
+ * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * CONTRIBUTORS:
- * 		Henrik Rentz-Reichert (initial contribution)
+ * 		Rohit Agrawal (initial contribution)
+ * 
  * 
  *******************************************************************************/
 
