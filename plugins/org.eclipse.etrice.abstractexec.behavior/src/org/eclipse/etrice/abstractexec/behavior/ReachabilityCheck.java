46ed5e424ec9adb3b915e421894970db0ac93046
Juergen Haug <juergen.markus.haug@student.uni-augsburg.de>
2012-12-19 14:27:30 +0100

    [abstractexec] added reachability tests
    Change-Id: Ieab8161d6d69a9c9f6c1c363b4df2ace54803516

--- plugins/org.eclipse.etrice.abstractexec.behavior/src/org/eclipse/etrice/abstractexec/behavior/ReachabilityCheck.java
+++ plugins/org.eclipse.etrice.abstractexec.behavior/src/org/eclipse/etrice/abstractexec/behavior/ReachabilityCheck.java
@@ -11,133 +11,124 @@
  * 
  *******************************************************************************/
 
-
 package org.eclipse.etrice.abstractexec.behavior;
 
 import java.util.*;
 
-
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.etrice.core.genmodel.etricegen.ActiveTrigger;
 import org.eclipse.etrice.core.genmodel.etricegen.ExpandedActorClass;
 import org.eclipse.etrice.core.room.*;
 import org.eclipse.etrice.core.room.util.RoomHelpers;
-import org.eclipse.etrice.generator.base.DefaultTranslationProvider;
-import org.eclipse.etrice.generator.base.DetailCodeTranslator;
-
 
 public class ReachabilityCheck {
-Queue<StateGraphNode> queue;
-public Set<StateGraphItem> visited;
-private ExpandedActorClass xpAct;
-private Set<StateGraphItem> exitUsed;
-public ReachabilityCheck (ExpandedActorClass xpac) {
-	queue= new LinkedList<StateGraphNode>();
-	xpAct = xpac;
-	visited = new HashSet<StateGraphItem>();
-	exitUsed = new HashSet<StateGraphItem>();
-}
-public void computeReachability() {
-	StateGraph graph = xpAct.getStateMachine();
-	addStartingPoints(graph, true);
-	doTraversal();
-}
+	Queue<StateGraphNode> queue;
+	public Set<StateGraphItem> visited;
+	private ExpandedActorClass xpAct;
+	private Set<StateGraphItem> exitUsed;
 
-private void addStartingPoints(StateGraph graph, boolean add_initial) 
-{
-	
-	EList<Transition> transitions = graph.getTransitions();
-	EList<TrPoint> trPoint = graph.getTrPoints();
-	if(add_initial)
-	for(Transition trans  : transitions)
-		if(trans instanceof InitialTransition) 
-		{ 
-			visited.add(trans);
-			StateGraphNode cur = xpAct.getNode(trans.getTo());
-			if(!visited.contains(cur)) queue.add(cur); 
-			break;
-		}
-	for(TrPoint tp : trPoint)
-	{
-		if(tp instanceof TransitionPoint && !visited.contains(tp) ) {
-			queue.add(tp);
-		}
+	public ReachabilityCheck(ExpandedActorClass xpac) {
+		queue = new LinkedList<StateGraphNode>();
+		xpAct = xpac;
+		visited = new HashSet<StateGraphItem>();
+		exitUsed = new HashSet<StateGraphItem>();
 	}
-	
-	//add transition points
-}
-private void doTraversal() {
-	while(!queue.isEmpty())
-	{
-		StateGraphNode node = queue.poll();
-		Visit(node);
-		//System.out.println("Visited node : " + node.getName());
+
+	public void computeReachability() {
+		StateGraph graph = xpAct.getStateMachine();
+		addStartingPoints(graph, true);
+		doTraversal();
 	}
-}
-public boolean isExitUsed(StateGraphItem item)
-{
-	return exitUsed.contains(item);
-}
-public boolean isReachable(StateGraphItem item)
-{
-	
-	return visited.contains(item);
-}
-private void Visit(StateGraphNode node)
-{
-	visited.add(node);
-	if(node instanceof State)
-	{
-		State st = (State) node;
-		if(RoomHelpers.hasDirectSubStructure(st))
-		{
-			addStartingPoints(st.getSubgraph(),true);
-		}
-		else
-		{
-			for(ActiveTrigger trigger : xpAct.getActiveTriggers(st))
-			{
-				for(Transition trans : trigger.getTransitions())
-				{
+
+	private void addStartingPoints(StateGraph graph, boolean add_initial) {
+
+		EList<Transition> transitions = graph.getTransitions();
+		EList<TrPoint> trPoint = graph.getTrPoints();
+		if (add_initial)
+			for (Transition trans : transitions)
+				if (trans instanceof InitialTransition) {
 					visited.add(trans);
-					StateGraphNode target = xpAct.getNode(trans.getTo());
-					if(!visited.contains(target)) {
-						queue.add(target);
-						StateGraph triggerContext = (StateGraph) trans.eContainer();
-						State exitCalled = st;
-						while(true)
-						{
-							exitUsed.add(exitCalled);
-							if(exitCalled.eContainer()==triggerContext) break;
-							exitCalled = (State) exitCalled.eContainer().eContainer();
+					StateGraphNode cur = xpAct.getNode(trans.getTo());
+					if (!visited.contains(cur))
+						queue.add(cur);
+					break;
+				}
+		for (TrPoint tp : trPoint) {
+			if (tp instanceof TransitionPoint && !visited.contains(tp)) {
+				queue.add(tp);
+			}
+		}
+
+		// add transition points
+	}
+
+	private void doTraversal() {
+		while (!queue.isEmpty()) {
+			StateGraphNode node = queue.poll();
+			if (!visited.contains(node))
+				Visit(node);
+			// System.out.println("Visited node : " + node.getName());
+		}
+	}
+
+	public boolean isExitUsed(StateGraphItem item) {
+		return exitUsed.contains(item);
+	}
+
+	public boolean isReachable(StateGraphItem item) {
+
+		return visited.contains(item);
+	}
+
+	private void Visit(StateGraphNode node) {
+		visited.add(node);
+		if (node instanceof State) {
+			State st = (State) node;
+			if (RoomHelpers.hasDirectSubStructure(st)) {
+				addStartingPoints(st.getSubgraph(), true);
+			} else {
+				for (ActiveTrigger trigger : xpAct.getActiveTriggers(st)) {
+					for (Transition trans : trigger.getTransitions()) {
+						visited.add(trans);
+						StateGraphNode target = xpAct.getNode(trans.getTo());
+						if (!visited.contains(target)) {
+							queue.add(target);
+							StateGraph triggerContext = (StateGraph) trans
+									.eContainer();
+							State exitCalled = st;
+							while (true) {
+								exitUsed.add(exitCalled);
+								if (exitCalled.eContainer() == triggerContext)
+									break;
+								exitCalled = (State) exitCalled.eContainer()
+										.eContainer();
+							}
 						}
 					}
 				}
 			}
-		}
-	}
-	else {
-		if(node instanceof EntryPoint)
-		{
-			State container = (State) node.eContainer().eContainer();
-			visited.add(container);
-		}
-		for(Transition trans : xpAct.getOutgoingTransitions(node))
-		{
-			visited.add(trans);
-			StateGraphNode target = xpAct.getNode(trans.getTo());
-			if(!visited.contains(target))
-			{
-				queue.add(target);
-				
+		} else {
+			if (node instanceof EntryPoint) {
+				// if container has no initial transition then mark it visited
+				boolean markVisited = true;
+				State container = (State) node.eContainer().eContainer();
+				StateGraph parentGraph = container.getSubgraph();
+				for (Transition tr : parentGraph.getTransitions())
+					if (tr instanceof InitialTransition)
+						markVisited = false;
+				if (markVisited)
+					visited.add(container);
+			}
+			for (Transition trans : xpAct.getOutgoingTransitions(node)) {
+				visited.add(trans);
+				StateGraphNode target = xpAct.getNode(trans.getTo());
+				if (!visited.contains(target)) {
+					queue.add(target);
+
+				}
 			}
 		}
+
 	}
-	
-}
-
 
 }
-
