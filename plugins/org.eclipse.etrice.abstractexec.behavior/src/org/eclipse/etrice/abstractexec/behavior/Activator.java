36a897a523bb2f0549d96e4f3f1e4844b7f34599
rohit21agrawal <rohi0012@e.ntu.edu.sg>
2012-12-20 22:23:28 +0530

    [CQ6589]: All files, committed from scratch due to resetting of environment
    1.	I authored 100% of the content I contributed 
    2.	I have the rights to donate the content to Eclipse 
    3.	I contribute the content under the EPL 
    
    
    Change-Id: I073d19dca95528603fc2af7fe585f131e9901377

---
+++ plugins/org.eclipse.etrice.abstractexec.behavior/src/org/eclipse/etrice/abstractexec/behavior/Activator.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Rohit Agrawal
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * CONTRIBUTORS:
+ * 		Rohit Agrawal (initial contribution)
+ * 
+ *******************************************************************************/
+
+package org.eclipse.etrice.abstractexec.behavior;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends AbstractUIPlugin implements BundleActivator {
+
+	private static Activator instance = null;
+	
+	public static 
+	Activator getDefault() {
+		return instance;
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		instance = this;
+	}
+	
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		instance = null;
+		super.stop(context);
+	}
+}
---
+++ plugins/org.eclipse.etrice.abstractexec.behavior/src/org/eclipse/etrice/abstractexec/behavior/ProposalGenerator.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Rohit Agrawal
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * CONTRIBUTORS:
+ * 		Rohit Agrawal (initial contribution)
+ * 
+ *******************************************************************************/
+
+package org.eclipse.etrice.abstractexec.behavior;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.etrice.core.genmodel.etricegen.ActiveTrigger;
+import org.eclipse.etrice.core.genmodel.etricegen.ExpandedActorClass;
+import org.eclipse.etrice.core.room.InterfaceItem;
+import org.eclipse.etrice.core.room.MessageFromIf;
+import org.eclipse.etrice.core.room.Port;
+import org.eclipse.etrice.core.room.RoomFactory;
+import org.eclipse.etrice.core.room.SemanticsRule;
+import org.eclipse.etrice.core.room.State;
+import org.eclipse.etrice.core.room.util.RoomHelpers;
+
+public class ProposalGenerator {
+	private ExpandedActorClass xpac;
+	private SemanticsCheck checker;
+	private List<MessageFromIf> outgoingProposal = new LinkedList<MessageFromIf>();
+	private List<MessageFromIf> incomingProposal = new LinkedList<MessageFromIf>();
+	private static boolean traceProposals = false;
+	static {
+		if (Activator.getDefault().isDebugging()) {
+			String value = Platform
+					.getDebugOption("org.eclipse.etrice.abstractexec.behavior/trace/proposals");
+			if (value != null && value.equalsIgnoreCase(Boolean.toString(true))) {
+				traceProposals = true;
+			}
+		}
+	}
+
+	public ProposalGenerator(ExpandedActorClass xp, SemanticsCheck chk) {
+		xpac = xp;
+		checker = chk;
+	}
+
+	public List<MessageFromIf> getIncomingProposals() {
+		return incomingProposal;
+	}
+
+	public List<MessageFromIf> getOutgoingProposals() {
+		return outgoingProposal;
+	}
+
+	
+	public void createProposals(State st) {
+		ActiveRules rules = checker.getActiveRules(st);
+
+		// in case the state is disconnected component of the graph
+		if (rules == null)
+			return;
+
+		outgoingProposal.clear();
+		incomingProposal.clear();
+
+		Set<SemanticsRule> rulesToIgnore = new HashSet<SemanticsRule>();
+		
+			for (ActiveTrigger trigger : xpac.getActiveTriggers(st)) {
+				SemanticsRule match = null;
+				Port port = (Port) trigger.getIfitem();
+				if (rules.getPortList().contains(port)) {
+					List<SemanticsRule> ruleList = rules.getRulesForPort(port);
+					for (SemanticsRule curRule : ruleList) {
+						if (curRule.getMsg() == trigger.getMsg()) {
+							match = curRule;
+							break;
+						}
+					}
+				}
+				if (match != null) {
+					// mark this rule for ignoring while generating proposals
+					// as they have already been taken care of
+					rulesToIgnore.add(match);
+				} 
+			}
+
+		// now start generating proposals by listing all the rules and ignoring
+		// the ones
+		// marked above
+		for (InterfaceItem item : rules.getPortList()) {
+			for (SemanticsRule ruleToCheck : rules.getRulesForPort(item)) {
+				if (!rulesToIgnore.contains(ruleToCheck)) {
+					MessageFromIf mif = RoomFactory.eINSTANCE
+							.createMessageFromIf();
+					mif.setFrom(item);
+					mif.setMessage(ruleToCheck.getMsg());
+					boolean isOutgoing = RoomHelpers.getMessageListDeep(item, true)
+							.contains(ruleToCheck.getMsg());
+					if (isOutgoing) {
+						outgoingProposal.add(mif);
+					} else {
+						incomingProposal.add(mif);
+					}
+
+				}
+			}
+		}
+
+		if (traceProposals) {
+			System.out.println("  Proposals for : " + st.getName());
+
+			for (MessageFromIf msg : outgoingProposal) {
+				System.out.println("    Outgoing msg proposal : "
+						+ msg.getFrom().getName() + "."
+						+ msg.getMessage().getName() + "()");
+			}
+			for (MessageFromIf msg : incomingProposal) {
+				System.out.println("    Incoming msg proposal : "
+						+ msg.getMessage().getName() + " from "
+						+ msg.getFrom().getName());
+			}
+		}
+	}
+
+}
