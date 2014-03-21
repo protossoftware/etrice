7f19845bc17cf5f32d17b8964eb4d7b659c9e7ef
Henrik Rentz-Reichert <hrr@protos.de>
2014-03-21 17:14:27 +0100

    [generator] slightly generalized the IntelligentSeparator
    Change-Id: Ifbaac77284290869346dea79fb3d147fe4524907

--- plugins/org.eclipse.etrice.generator/src/org/eclipse/etrice/generator/base/IntelligentSeparator.java
+++ plugins/org.eclipse.etrice.generator/src/org/eclipse/etrice/generator/base/IntelligentSeparator.java
@@ -14,21 +14,34 @@ package org.eclipse.etrice.generator.base;
 
 /**
  * This class implements an intelligent separator for continued
- * enumerations that are generated from multiple list where each of
+ * enumerations that are generated from multiple lists where each of
  * those list might be empty.
  * 
  * @author Henrik Rentz-Reichert
  */
 public class IntelligentSeparator {
 
+	private String firstSep;
 	private String sep;
 	private boolean firstUse = true;
+
+	/**
+	 * returns {@code firstSep} on the first call to {@link #toString()}.
+	 * The following times it return {@code sep}.
+	 * 
+	 * @param firstSep the first separator string to be used
+	 * @param sep the separator string to be used
+	 */
+	public IntelligentSeparator(String firstSep, String sep) {
+		this.firstSep = firstSep;
+		this.sep = sep;
+	}
 
 	/**
 	 * @param sep the separator string to be used
 	 */
 	public IntelligentSeparator(String sep) {
-		this.sep = sep;
+		this("", sep);
 	}
 
 	/**
@@ -52,7 +65,7 @@ public class IntelligentSeparator {
 	public String toString() {
 		if (firstUse) {
 			firstUse = false;
-			return "";
+			return firstSep;
 		}
 		else
 			return sep;
