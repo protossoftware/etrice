a536a5baffe631964f650c0f625d7beb20ccc62e
Juergen Haug <juergen.haug@protos.de>
2015-05-29 14:26:45 +0200

    [wizard] auto runtime/modellib import
    Change-Id: I0581e26d6ae0c253c0eb281591e95b08eeab87fc

---
+++ plugins/org.eclipse.etrice.ui.runtime/src/org/eclipse/etrice/ui/runtime/NonDeletingWizardInstaller.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * Copyright (c) 2015 protos software gmbh (http://www.protos.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * CONTRIBUTORS:
+ * 		Juergen Haug (initial contribution)
+ * 
+ *******************************************************************************/
+
+package org.eclipse.etrice.ui.runtime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.ui.CommonUIPlugin;
+import org.eclipse.emf.common.ui.wizard.ExampleInstallerWizard;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.jface.resource.ImageDescriptor;
+
+public class NonDeletingWizardInstaller extends ExampleInstallerWizard {
+
+	protected boolean isPerformingFinish = false;
+	
+	@Override
+	protected Diagnostic deleteExistingProjects(IProgressMonitor monitor) {
+		return Diagnostic.OK_INSTANCE;
+	}
+	
+	@Override
+	public boolean performFinish() {
+		isPerformingFinish = true;
+		return super.performFinish();
+	}
+	
+	@Override
+	protected List<ProjectDescriptor> getProjectDescriptors() {
+		if(!isPerformingFinish)
+			return super.getProjectDescriptors();
+		
+		List<ProjectDescriptor> newProjects = new ArrayList<ProjectDescriptor>();
+		for(ProjectDescriptor desc : super.getProjectDescriptors())
+			if(!desc.getProject().exists())
+				newProjects.add(desc);
+		
+		return newProjects;
+	}
+
+	@Override
+	public void addPages() {
+		projectPage = new CustomProjectPage("projectPage", CommonUIPlugin.INSTANCE.getString("_UI_ProjectPage_title"),
+				null);
+		projectPage.setDescription(CommonUIPlugin.INSTANCE.getString("_UI_ProjectPage_description"));
+		addPage(projectPage);
+	}
+
+	class CustomProjectPage extends ProjectPage {
+
+		public CustomProjectPage(String pageName, String title, ImageDescriptor titleImage) {
+			super(pageName, title, titleImage);
+		}
+
+		@Override
+		protected void itemSelected() {
+			renameButton.setEnabled(false);
+
+			ProjectDescriptor projectDescriptor = getSelectedProjectDescriptor();
+			if (projectDescriptor != null) {
+				boolean exists = projectDescriptor.getProject().exists();
+				String description = projectDescriptor.getDescription() != null ? projectDescriptor.getDescription()
+						: "";
+				if (exists) {
+					String renameMessage = "* A project with this name already exists in the workspace and will not be added.";
+					description = description == "" ? renameMessage : CommonUIPlugin.INSTANCE.getString(
+							"_UI_ProjectDescriptionAndRename_message", new String[] { description, renameMessage });
+				}
+				descriptionText.setText(description);
+			}
+		}
+
+	}
+}
