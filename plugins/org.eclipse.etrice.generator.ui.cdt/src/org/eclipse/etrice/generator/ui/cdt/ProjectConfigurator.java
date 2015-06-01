aa0ca092039533bcd4388ba4b75a4ffe3b1a685c
Juergen Haug <juergen.haug@protos.de>
2015-06-01 13:19:51 +0200

    Bug 467892 - [runtime.c] New Set of eTrice models adds linux libraries
    Change-Id: Ic42a61195b05f215e28cb53f05931235dcc9264f

--- plugins/org.eclipse.etrice.generator.ui.cdt/src/org/eclipse/etrice/generator/ui/cdt/ProjectConfigurator.java
+++ plugins/org.eclipse.etrice.generator.ui.cdt/src/org/eclipse/etrice/generator/ui/cdt/ProjectConfigurator.java
@@ -1,12 +1,14 @@
 package org.eclipse.etrice.generator.ui.cdt;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.cdt.core.model.CoreModel;
 import org.eclipse.cdt.core.settings.model.CIncludePathEntry;
+import org.eclipse.cdt.core.settings.model.CLibraryFileEntry;
 import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
 import org.eclipse.cdt.core.settings.model.ICLanguageSetting;
 import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
@@ -14,72 +16,94 @@ import org.eclipse.cdt.core.settings.model.ICProjectDescription;
 import org.eclipse.cdt.core.settings.model.ICSettingEntry;
 import org.eclipse.cdt.managedbuilder.core.IConfiguration;
 import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.etrice.generator.ui.configurator.IProjectConfigurator;
 
+/*
+ * Docs:
+ * http://cdt-devel-faq.wikidot.com/
+ * 
+ * CDataUtil.addExcludePaths(entry, paths, removePrefix)
+ */
 public class ProjectConfigurator implements IProjectConfigurator {
-	
-	final static String MINGW_TOOLCHAIN_NAME = "MinGW GCC";
-	final static String POSIX_TOOLCHAIN_NAME = "Linux GCC";
-	
+
+	final static String MINGW_TOOLCHAIN = "MinGW GCC";
+	final static String POSIX_TOOLCHAIN = "Linux GCC";
+
 	public ProjectConfigurator() {
 	}
 
-	
 	@Override
 	public void configure(IProject project) {
 		try {
-			referenceETriceProjects(project);
+			if (!CoreModel.hasCNature(project))
+				return;
+			
+			configureIncludesAndLibraries(project);
 		}
 		catch (CoreException e) {
 			e.printStackTrace();
 		}
 	}
 
-	/*
-	 * HOWTO: Find out reference project configuration: Use GUI "Reference" tab at "Path and Symbols" and set breakpoint at {@link org.eclipse.cdt.internal.core.settings.model.CConfigurationDescription#setReferenceInfo(Map)}
-	 */
-	public void referenceETriceProjects(IProject project) throws CoreException {
-		if (!project.hasNature("org.eclipse.cdt.core.cnature"))
-			return;
-
-		List<CIncludePathEntry> includes = new ArrayList<CIncludePathEntry>();
-		IFolder src_gen = project.getFolder("src-gen");
-		includes.add(new CIncludePathEntry(src_gen, ICSettingEntry.LOCAL));
+	public void configureIncludesAndLibraries(IProject project) throws CoreException {
 		
+		List<CIncludePathEntry> srcIncludes = new ArrayList<CIncludePathEntry>();
+		srcIncludes.add(new CIncludePathEntry("/${ProjName}/src-gen", ICSettingEntry.LOCAL));
+
+		List<CLibraryFileEntry> posixLibraries = new ArrayList<CLibraryFileEntry>();
+		posixLibraries.add(new CLibraryFileEntry("pthread", ICSettingEntry.LOCAL));
+		posixLibraries.add(new CLibraryFileEntry("rt", ICSettingEntry.LOCAL));
+
 		ICProjectDescription projectDescription = CoreModel.getDefault().getProjectDescription(project, true);
-		ICConfigurationDescription configDescriptions[] = projectDescription.getConfigurations();
-		for (ICConfigurationDescription configDescription : configDescriptions) {
+		// all build configurations e.g. Debug, Release
+		for (ICConfigurationDescription configDescription : projectDescription.getConfigurations()) {
 			if (configDescription.getId() == null)
 				continue;
-			
+
 			IConfiguration buildConfig = ManagedBuildManager.getConfigurationForDescription(configDescription);
-			if (MINGW_TOOLCHAIN_NAME.equals(buildConfig.getToolChain().getName())) {
+			String toolChain = "";
+			if (MINGW_TOOLCHAIN.equals(buildConfig.getToolChain().getName()))
+				toolChain = MINGW_TOOLCHAIN;
+			else if (POSIX_TOOLCHAIN.equals(buildConfig.getToolChain().getName()))
+				toolChain = POSIX_TOOLCHAIN;
+			// set project references
+			/*
+			 * HOWTO: Find out reference project configuration: Use GUI "Reference" tab
+			 * at "Path and Symbols" and set breakpoint at {@link
+			 * org.eclipse.cdt.internal.core.settings.model.CConfigurationDescription#setReferenceInfo(Map)}
+			 */
+			if (toolChain == MINGW_TOOLCHAIN) {
 				Map<String, String> projectRefInfo = configDescription.getReferenceInfo();
-				projectRefInfo.put("org.eclipse.etrice.runtime.c",
-						"cdt.managedbuild.config.gnu.mingw.lib.debug.1978608919");
-				projectRefInfo.put("org.eclipse.etrice.modellib.c",
-						"cdt.managedbuild.config.gnu.mingw.lib.debug.847049798");
+				projectRefInfo.put("org.eclipse.etrice.runtime.c", "cdt.managedbuild.config.gnu.mingw.lib.debug.1978608919");
+				projectRefInfo.put("org.eclipse.etrice.modellib.c", "cdt.managedbuild.config.gnu.mingw.lib.debug.847049798");
 				configDescription.setReferenceInfo(projectRefInfo);
 			}
-			else if (POSIX_TOOLCHAIN_NAME.equals(buildConfig.getToolChain().getName())) {
+			else if (toolChain == POSIX_TOOLCHAIN) {
 				Map<String, String> projectRefInfo = configDescription.getReferenceInfo();
 				projectRefInfo.put("org.eclipse.etrice.runtime.c", "cdt.managedbuild.config.gnu.mingw.lib.debug.1978608919.294295052");
 				projectRefInfo.put("org.eclipse.etrice.modellib.c", "cdt.managedbuild.config.gnu.mingw.lib.debug.847049798.58778989");
 				configDescription.setReferenceInfo(projectRefInfo);
 			}
 
+			// setting for compiler, linker, assembler (see id)
 			for (ICLanguageSetting setting : configDescription.getRootFolderDescription().getLanguageSettings()) {
-				if (!"org.eclipse.cdt.core.gcc".equals(setting.getLanguageId()))
+				if(setting.getId() == null)
 					continue;
+				
+				// set source includes
+				if (setting.getId().startsWith("cdt.managedbuild.tool.gnu.c.compiler"))
+					addSettings(setting, ICSettingEntry.INCLUDE_PATH, srcIncludes);
 
-				addSettings(setting, ICSettingEntry.INCLUDE_PATH, includes);
+				// set additional libraries (order!)
+				if (toolChain == POSIX_TOOLCHAIN) {
+					if (setting.getId().startsWith("cdt.managedbuild.tool.gnu.c.linker"))
+						addSettings(setting, ICSettingEntry.LIBRARY_FILE, posixLibraries);
+				}
 			}
-
 		}
-		
+
 		CoreModel.getDefault().setProjectDescription(project, projectDescription);
 	}
 
@@ -169,16 +193,24 @@ public class ProjectConfigurator implements IProjectConfigurator {
 	// e.printStackTrace();
 	// }
 	// }
-
-	private static void addSettings(ICLanguageSetting setting, int kind, List<? extends ICLanguageSettingEntry> entries) {
-		HashMap<String, ICLanguageSettingEntry> newEntries = new HashMap<String, ICLanguageSettingEntry>();
-		for (ICLanguageSettingEntry entry : setting.getSettingEntriesList(kind)) {
-			newEntries.put(entry.getName(), entry);
+	
+	/**
+	 *  Appends entries at the end, preserves order, prevents new duplicates.
+	 */
+	private void addSettings(ICLanguageSetting setting, int kind, List<? extends ICLanguageSettingEntry> newEntries){
+		Set<String> valueSet = new HashSet<String>();
+		for (ICLanguageSettingEntry entry : setting.getSettingEntriesList(kind))
+			valueSet.add(entry.getValue());
+		
+		List<ICLanguageSettingEntry> allEntries = new ArrayList<ICLanguageSettingEntry>();
+		for (ICLanguageSettingEntry entry : setting.getSettingEntriesList(kind))
+			allEntries.add(entry);
+		for (ICLanguageSettingEntry entry : newEntries){
+			if(!valueSet.contains(entry.getValue()))
+				allEntries.add(entry);
 		}
-		for (ICLanguageSettingEntry entry : entries) {
-			newEntries.put(entry.getName(), entry);
-		}
-		setting.setSettingEntries(kind, new ArrayList<ICLanguageSettingEntry>(newEntries.values()));
+			
+		setting.setSettingEntries(kind, allEntries);
 	}
 
 }
