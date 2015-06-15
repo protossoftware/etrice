c08bfcd38e0470066c8557ff3de2a949076a894d
Henrik Rentz-Reichert <hrr@protos.de>
2015-06-15 14:31:56 +0200

    Bug 468810 - hudson build automation for command line generators https://bugs.eclipse.org/bugs/show_bug.cgi?id=468810
    Changed to create jar with separate jars and zip all together
    
    Change-Id: I8b36a83aba94292f2939a8999f95e3c651a83120

--- releng/org.eclipse.etrice.releng/post-build.sh
+++ releng/org.eclipse.etrice.releng/post-build.sh
@@ -6,4 +6,5 @@ echo 'copy update site'
 cp -R ${WORKSPACE}/last_success/site.packed/* /home/data/httpd/download.eclipse.org/etrice/updates/nightly/
 echo 'copy zips'
 cp -R ${WORKSPACE}/last_success/site.p2.zip/* /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips/
+cp -R ${WORKSPACE}/last_success/deliveries /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips/
 echo 'done'
