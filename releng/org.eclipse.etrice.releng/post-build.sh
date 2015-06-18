87d21a19626ab3529c00c40be3c2b7e763b1bc3f
Henrik Rentz-Reichert <hrr@protos.de>
2015-06-18 14:01:10 +0200

    renamed deliveries -> generators
    Change-Id: Ife29568b6692965a91198696499501f5297e896a

--- releng/org.eclipse.etrice.releng/post-build.sh
+++ releng/org.eclipse.etrice.releng/post-build.sh
@@ -2,10 +2,12 @@ echo 'promoting to download area...'
 echo 'deleting directories'
 rm -Rf /home/data/httpd/download.eclipse.org/etrice/updates/nightly/*
 rm -Rf /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips/*
-rm -Rf /home/data/httpd/download.eclipse.org/etrice/archives/nightly/deliveries
+rm -Rf /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators
 echo 'copy update site'
 cp -R ${WORKSPACE}/last_success/site.packed/* /home/data/httpd/download.eclipse.org/etrice/updates/nightly/
 echo 'copy zips'
-cp -R ${WORKSPACE}/last_success/site.p2.zip/* /home/data/httpd/download.eclipse.org/etrice/archives/nightly/
-cp -R ${WORKSPACE}/last_success/deliveries /home/data/httpd/download.eclipse.org/etrice/archives/nightly/
+cp -R ${WORKSPACE}/last_success/site.p2.zip/* /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips
+mkdir /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators
+cp ${WORKSPACE}/last_success/deliveries/eTrice-C-generator.zip /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators/eTrice-C-generator.zip
+cp ${WORKSPACE}/last_success/deliveries/eTrice-Java-generator.zip /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators/eTrice-Java-generator.zip
 echo 'done'
