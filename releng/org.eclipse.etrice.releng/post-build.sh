7a7f8a3685bd219abb674571a08c8c8de318b1d0
Henrik Rentz-Reichert <hrr@protos.de>
2015-06-18 13:43:49 +0200

    [releng] copy deliveries to archives/nightly (not to zips sub folder)
    since the former location caused troubles with the ForwardToDownload.php
    script when there are several items in the zips folder
    
    Change-Id: I104e6717a8117841c6d15a08ada4280e7c3ccf52

--- releng/org.eclipse.etrice.releng/post-build.sh
+++ releng/org.eclipse.etrice.releng/post-build.sh
@@ -2,9 +2,10 @@ echo 'promoting to download area...'
 echo 'deleting directories'
 rm -Rf /home/data/httpd/download.eclipse.org/etrice/updates/nightly/*
 rm -Rf /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips/*
+rm -Rf /home/data/httpd/download.eclipse.org/etrice/archives/nightly/deliveries
 echo 'copy update site'
 cp -R ${WORKSPACE}/last_success/site.packed/* /home/data/httpd/download.eclipse.org/etrice/updates/nightly/
 echo 'copy zips'
-cp -R ${WORKSPACE}/last_success/site.p2.zip/* /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips/
-cp -R ${WORKSPACE}/last_success/deliveries /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips/
+cp -R ${WORKSPACE}/last_success/site.p2.zip/* /home/data/httpd/download.eclipse.org/etrice/archives/nightly/
+cp -R ${WORKSPACE}/last_success/deliveries /home/data/httpd/download.eclipse.org/etrice/archives/nightly/
 echo 'done'
