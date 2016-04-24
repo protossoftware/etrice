81e0094dc2dcbbf2420a4ed362161d8802dddb02
Henrik Rentz-Reichert <hrr@protos.de>
2016-04-24 22:27:17 +0200

    post build copies cpp generator to downlaod area
    Change-Id: I7a10594599a7d4517ffba758a8b34a80a70faecc

--- releng/org.eclipse.etrice.releng/post-build.sh
+++ releng/org.eclipse.etrice.releng/post-build.sh
@@ -9,5 +9,6 @@ echo 'copy zips'
 cp -R ${WORKSPACE}/last_success/site.p2.zip/* /home/data/httpd/download.eclipse.org/etrice/archives/nightly/zips
 mkdir /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators
 cp ${WORKSPACE}/last_success/deliveries/eTrice-C-generator.zip /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators/eTrice-C-generator.zip
+cp ${WORKSPACE}/last_success/deliveries/eTrice-CPP-generator.zip /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators/eTrice-CPP-generator.zip
 cp ${WORKSPACE}/last_success/deliveries/eTrice-Java-generator.zip /home/data/httpd/download.eclipse.org/etrice/archives/nightly/generators/eTrice-Java-generator.zip
 echo 'done'
