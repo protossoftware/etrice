d7cccfb959d571175ff2c2be175e871de529e90a
Juergen Haug <juergen.haug@protos.de>
2017-05-19 13:59:15 +0200

    readded effect of RoomConvertingLazyLinker
    API is available since 2.7
    
    Change-Id: Id86aede6f9254c05b47b8c64596ea770f744bc96

--- plugins/org.eclipse.etrice.core.room/src/org/eclipse/etrice/core/linking/RoomConvertingLazyLinker.java
+++ plugins/org.eclipse.etrice.core.room/src/org/eclipse/etrice/core/linking/RoomConvertingLazyLinker.java
@@ -12,27 +12,35 @@
 
 package org.eclipse.etrice.core.linking;
 
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.etrice.core.fsm.fSM.FSMPackage;
+import org.eclipse.etrice.core.room.RoomPackage;
 import org.eclipse.xtext.linking.lazy.LazyLinker;
 
 /**
- * Linker that converts incompatible super classes to corresponding room sub classes
+ * Linker that uses concrete ROOM sub classes instead of FSM super classes.
  */
 public class RoomConvertingLazyLinker extends LazyLinker {
 
-//	protected static FSMPackage fsm = FSMPackage.eINSTANCE;
-//	protected static RoomPackage room = RoomPackage.eINSTANCE;
-//		
-//	@Override
-//	protected EClass getProxyType(EObject obj, EReference eRef) {
-//		EClass orig = super.getProxyType(obj, eRef);
-//		
-//		if(orig == fsm.getModelComponent())
-//			return room.getActorClass();
-//		if(orig == fsm.getAbstractInterfaceItem())
-//			return room.getInterfaceItem();
-////		if(orig == room.getGeneralProtocolClass())
-////			return ? => cannot decide, bad
-//		
-//		return orig;
-//	}
+	protected static FSMPackage fsm = FSMPackage.eINSTANCE;
+	protected static RoomPackage room = RoomPackage.eINSTANCE;
+		
+	@Override
+	protected EClass getProxyType(EObject obj, EReference eRef) {
+		EClass orig = super.getProxyType(obj, eRef);
+		
+		// use concrete room classes
+		if(orig == fsm.getModelComponent())
+			return room.getActorClass();
+		if(orig == fsm.getAbstractInterfaceItem())
+			return room.getInterfaceItem();
+		
+		
+//		if(orig == room.getGeneralProtocolClass())
+//			return ? => cannot decide
+		
+		return orig;
+	}
 }
